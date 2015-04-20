7b2d5a7d9ec8192ef7a6ba90e22aaec4781f347e
wbcmac2000 <wbcmac2000@gmail.com>
2015-04-19 21:47:12 -0400

    mainFile

---
+++ add.bat
@@ -0,0 +1,14 @@
+@color 1a
+@title Christmas adder
+@echo Made by wbc who is lazy
+@timeout /t 5
+@color 2c
+@cls
+@set /p item=Enter the thing:
+@echo Thank you
+@timeout /t 3 > nul
+@echo Please wait. Adding...
+@timeout /t 4 > nul
+@echo 95%item% >> ChristmasList.txt
+@echo Done
+@timeout /t 5
